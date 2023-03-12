d87d546f50859922531008037f182237b860d346
Gammarkin <you@example.com>
2023-03-11 21:34:53 -0300

    adds header to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-Hey, listen!
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 }
 
