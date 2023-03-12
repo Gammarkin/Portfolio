3c855043115f2b210c45a5d0090174c8e552f0cd
Gammarkin <you@example.com>
2023-03-11 21:34:12 -0300

    adds name and pR to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import { HashLink } from "react-router-hash-link";
 export default function Header() {
   return (
     <header>
+      <p>Marcos Roberto</p>
+      <p>Full-Stack Developer</p>
+
       <ul>
         <li>
           <HashLink to="/#home">Home</HashLink>
