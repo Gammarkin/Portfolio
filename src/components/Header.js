6a0664a40966557974f07dae656ce8211412cd5e
Gammarkin <you@example.com>
2023-03-11 22:23:48 -0300

    adds header basic css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,20 @@ import { HashLink } from "react-router-hash-link";
 
 export default function Header() {
   return (
-    <header>
-      <p>Marcos Roberto</p>
-      <p>Full-Stack Developer</p>
+    <header className="header__container">
+      <section className="header__developer">
+      <p className="header__developer_name">Marcos Roberto</p>
+      <p className="header__developer_position">Full-Stack Developer</p>
+      </section>
 
-      <ul>
-        <li>
+      <ul className="header__link_container">
+        <li className="header__link">
           <HashLink to="/#home">Home</HashLink>
         </li>
-        <li>
+        <li className="header__link">
           <HashLink to="/#about">About</HashLink>
         </li>
-        <li>
+        <li className="header__link">
           <HashLink to="/#contact">Contact</HashLink>
         </li>
       </ul>
