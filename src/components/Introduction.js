050c9fe0a9f49c616e516eb2e14454340c4fd1ee
gammarkin <marcosrbnsf@gmail.com>
2023-03-22 23:26:43 -0300

    adds intro message to introduction component

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Saturn from "./Saturn";
+import IntroMessage from "./IntroMessage";
 
 export default function Introduction() {
   return (
     <section className="intro__container">
+      <IntroMessage />
       <Saturn />
     </section>
   )
