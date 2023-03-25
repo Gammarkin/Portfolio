14cea3ef4eecdd81232e54d1980a1a36f06b80ec
gammarkin <marcosrbnsf@gmail.com>
2023-03-24 21:12:34 -0300

    refactor: changes intro message

--- src/components/IntroMessage.js
+++ src/components/IntroMessage.js
@@ -4,7 +4,9 @@ export default function IntroMessage() {
     return (
         <section className='intro__message_container'>
             <p className='intro__message'>
-                Resolvendo problemas com código e criatividade.
+                Resolvendo problemas com código e
+                <br />
+                com uma criatividade de outro mundo.
             </p>
         </section>
     )
