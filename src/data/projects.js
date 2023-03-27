a219eaa467f612adfc4d2b5d6c2d7f1a92d713c7
gammarkin <marcosrbnsf@gmail.com>
2023-03-27 19:31:58 -0300

    feat: adds project data

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+import tfcImage from '../assets/images/front-example-tfc.png'
+import recipeAppImage from '../assets/images/recipe-app.png'
+import diceyImage from '../assets/images/dicey.png'
+
+const projects = [
+    {
+        image: tfcImage,
+        link: 'https://github.com/gammarkin/Try-Futebol-Clube',
+        description: 'a front-end page for a soccer club',
+    },
+    {
+        image: recipeAppImage,
+        link: 'https://github.com/gammarkin/recipe-app',
+        description: 'a recipe front-end page',
+    },
+    {
+        image: diceyImage,
+        link: 'https://github.com/gammarkin/dicey',
+        description: 'a discord bot built with discord.js for the system of ordem paranormal.'
+    }
+]
+
+export default projects
