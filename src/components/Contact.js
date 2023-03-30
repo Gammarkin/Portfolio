35ecb4810a1d447a9c62f67c7d9a0b51f70cbbc0
gammarkin <marcosrbnsf@gmail.com>
2023-03-30 18:19:46 -0300

    feat: adds contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import linkedinIcon from '../assets/images/linkedin.png';
+import githubIcon from '../assets/images/github-sign.png';
 
 export default function Contact() {
   return (
-    <div className='contact__container' id='contact'>Contact</div>
-  )
+    <section className="contact__container" id="contact">
+      <span className="contact__msg">Interessado em trabalhar comigo?</span>
+
+      <span className="contact__msg">
+        me mande um email {' '}
+        <a className="contact__email" href="mailto:marcosrbnsf@gmail.com">
+          marcosrbnsf@gmail.com
+        </a>
+      </span>
+
+      <div className="contact__links">
+        <a
+          href="https://www.linkedin.com/in/mknmarcosfilho/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="contact__icon" src={linkedinIcon} alt="linkedin" />
+        </a>
+
+        <a
+          href="github.com/gammarkin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="contact__icon" src={githubIcon} alt="github" />
+        </a>
+      </div>
+    </section>
+  );
 }
