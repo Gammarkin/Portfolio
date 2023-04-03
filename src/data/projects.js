9f5df2252563b85668b89553714edd76fec9cd0d
gammarkin <marcosrbnsf@gmail.com>
2023-04-03 20:35:45 -0300

    feat: adds planets to data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,24 +2,31 @@ import tfcImage from '../assets/images/front-example-tfc.png'
 import recipeAppImage from '../assets/images/recipe-app.png'
 import diceyImage from '../assets/images/dicey.png'
 
+import Venus from '../components/Venus'
+import Mars from '../components/Mars'
+import Uranus from '../components/Uranus'
+
 const projects = [
     {
         image: tfcImage,
         link: 'https://github.com/gammarkin/Try-Futebol-Clube',
-        description: 'a front-end page for a soccer club',
-        title: 'Try Futebol Clube app'
+        description: 'uma página front-end para um clube de futebol',
+        title: 'Try Futebol Clube app',
+        planet: Uranus,
     },
     {
         image: recipeAppImage,
         link: 'https://github.com/gammarkin/recipe-app',
-        description: 'a recipe front-end page',
-        title: 'Recipe App'
+        description: 'Um aplicativo de receitas',
+        title: 'Recipe App',
+        planet: Mars,
     },
     {
         image: diceyImage,
         link: 'https://github.com/gammarkin/dicey',
-        description: 'a discord bot built with discord.js for the system of ordem paranormal.',
-        title: 'Dicey bot'
+        description: 'um bot para o discord feito com discord.js para o sistema de ordem paranormal.',
+        title: 'Dicey bot',
+        planet: Venus,
     }
 ]
 
